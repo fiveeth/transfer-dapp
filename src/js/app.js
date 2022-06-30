3e3d501c82072d9b4faf463307197e3c8704f716
guoyihua <1573338993@qq.com>
2022-06-30 09:50:24 +0800

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,47 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: async function() {
+    return await App.initWeb3();
+  },
+
+  initWeb3: async function() {
+    
+
+    return App.initContract();
+  },
+
+  initContract: function() {
+    /*
+     * Replace me...
+     */
+
+    return App.bindEvents();
+  },
+
+  bindEvents: function() {
+    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  },
+
+  markAdopted: function() {
+    /*
+     * Replace me...
+     */
+  },
+
+  handleAdopt: function(event) {
+    event.preventDefault();
+
+    /*
+     * Replace me...
+     */
+  }
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
